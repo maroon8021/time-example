de030868b5ab4750064d45c6eea4e81b7082a45d
maroon8021 <okihiutat.iutim@gmail.com>
2021-04-19 18:06:54 +0900

    Fix: ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,11 @@
-const { DB_URL } = process.env;
+const { DB_URL, DB_MIGRATION } = process.env;
+
+const isDbMigration = DB_MIGRATION === "true";
 
 module.exports = {
   type: "postgres",
-  entities: ["src/models/**/*.ts"],
-  migrations: ["migrations/**/*.ts"],
+  entities: isDbMigration ? ["src/models/**/*.ts"] : ["dist/models/**/*.js"],
+  migrations: isDbMigration ?["migrations/**/*.ts"] :[],
 
   logging: true,
   synchronize: false,
